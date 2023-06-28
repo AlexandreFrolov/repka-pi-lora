c7390446340143b49bebfa1cf737f0eac7579b81
Alexandre Frolov <alexandre@frolov.pp.ru>
2023-06-28 10:45:52 +0000

    Fix

--- set-e32-config.py
+++ set-e32-config.py
@@ -1,8 +1,28 @@
 #!/usr/bin/python3
 # -*- coding: UTF-8 -*-
 
-#import RPi.GPIO as GPIO
-import RepkaPi.GPIO as GPIO
+def get_board_type():
+    try:
+        with open('/proc/device-tree/model', 'r') as f:
+            model = f.read().strip()
+            if 'Raspberry Pi' in model:
+                return 'Raspberry Pi'
+            elif 'Repka-Pi' in model:
+                return 'Repka-Pi'
+            else:
+                return 'Unknown'
+    except IOError:
+        return 'Unknown'
+
+board_type = get_board_type()
+
+if board_type == 'Repka-Pi':
+    import RepkaPi.GPIO as GPIO
+    GPIO.setboard(GPIO.REPKAPI3)
+elif board_type == 'Raspberry Pi':
+    import RPi.GPIO as GPIO
+print('Работает на платформе: ' + board_type + "\n")
+
 import serial
 import time
 import sys
@@ -13,8 +33,12 @@ NODE_CFG = [b'\xC0\x00\x0B\x1A\x0F\xC7', # 1: Node 1, Address 0x0B
             b'\xC0\x00\x0D\x1A\x0F\xC7', # 3: Node 3, Address 0x0D
             b'\xC0\x00\x0E\x1A\x0F\xC7'] # 4: Node 4, Address 0x0E
 
+def wait_for_aux_pin():
+    AUX = 18
+    while not GPIO.input(AUX):
+        sleep(0.04)
+
 def gpio_init ():
-    GPIO.setboard(GPIO.REPKAPI3)
     GPIO.setmode(GPIO.BCM)
     GPIO.setwarnings(False)
     M0 = 22
@@ -26,14 +50,19 @@ def gpio_init ():
     GPIO.output(M0,GPIO.HIGH)
     GPIO.output(M1,GPIO.HIGH)
     time.sleep(0.1)
-#    ser = serial.Serial("/dev/serial0", 9600)
-    ser = serial.Serial("/dev/ttyS0", 9600)
+   
+    if board_type == 'Repka-Pi':
+        ser = serial.Serial("/dev/ttyS0", 9600, timeout=1)
+    elif board_type == 'Raspberry Pi':
+        ser = serial.Serial("/dev/serial0", 9600, timeout=1)       
+        
     ser.flushInput()
     return ser
 
 def set_config(ser, node_id, new_cfg):
     try :
         if ser.isOpen() :
+            wait_for_aux_pin()
             ser.write(new_cfg)
             time.sleep(1)
     except :
@@ -43,11 +72,13 @@ def set_config(ser, node_id, new_cfg):
     ser.flushInput()
     try :
         if ser.isOpen() :
+            wait_for_aux_pin()
             ser.write(b'\xC1\xC1\xC1')
     except :
         if ser.isOpen() :
             ser.close()
         GPIO.cleanup()
+    wait_for_aux_pin()
     received_data = ser.read(6)
     sleep(0.03)
     print('Node ' + str(node_id) + ' new config:')
